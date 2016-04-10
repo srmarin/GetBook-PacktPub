e66a27419927e72c24a75f9fa41750685ee590be
Sergio <sergio_basket93@hotmail.com>
2016-04-10 15:30:06 +0200

    Eliminados print y comentarios innecesarios

--- getBook.py
+++ getBook.py
@@ -12,21 +12,17 @@ if contenido!= None:
 
 
 patron = re.compile('freelearning-claim\/[0-9]+\/[0-9]+')
-pagina = 'https://www.packtpub.com/'
 contenido = contenido.splitlines()
 
 for l in contenido:
-        #pos = lineas.find('<a href="/freelearning-claim/')
-        print l
-        #time.sleep(2)
         matcher = patron.search(l)
         if matcher:
             m = matcher.group(0)
-
-print m
 archivo.close()
 print "Archivo cerrado"
 
+pagina = 'https://www.packtpub.com/'
 pagina += m+'/'
 print pagina
+
 subprocess.call(['firefox',pagina])
